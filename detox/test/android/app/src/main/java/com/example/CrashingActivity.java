f21e5200fad4d841a9685fdd0b577bd9caa1ec02
Tâm Lê <tamle@Tams-MacBook-Pro.local>
2020-03-23 15:57:23 +0700

    remake repo

--- detox/test/android/app/src/main/java/com/example/CrashingActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class CrashingActivity extends MainActivity {
-    @Override
-    protected void onResume() {
-        super.onResume();
-        throw new IllegalStateException("This is an intentional crash!");
-    }
-}
--- scripts/ci.ios-release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-cd detox
-scripts/pack_ios.sh
